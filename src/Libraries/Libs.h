cd5fcf17bb6835ad9ca3f64ecad6515272719228
acrucha <mcaa@cin.ufpe.br>
2025-03-07 03:07:07 -0300

    feat(*): add Libraries

---
+++ src/Libraries/Libs.h
@@ -0,0 +1,25 @@
+#ifndef TRAINEE_CPP_LIBS_H
+#define TRAINEE_CPP_LIBS_H
+
+// C++ Standard Libraries (or <bits/stdc++.h> - https://www.geeksforgeeks.org/bitsstdc-h-c/)
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <limits>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+
+#endif /* TRAINEE_CPP_LIBS_H */
